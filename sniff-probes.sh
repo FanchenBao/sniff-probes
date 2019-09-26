69c0e5a7d8b7aa364efec8138d2ac5b368798e8f
FanchenBao <jonathonbao@gmail.com>
2019-09-26 11:06:28 -0400

    Revert back to bg in channel hop

--- sniff-probes.sh
+++ sniff-probes.sh
@@ -13,7 +13,7 @@ channel_hop() {
 	IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
 
 	while true ; do
-		for CHAN in $IEEE80211bga ; do
+		for CHAN in $IEEE80211bg ; do
 			# echo "switching $IFACE to channel $CHAN"
 			sudo iwconfig $IFACE channel $CHAN
 			sleep 1
